2afd41f218f47732a1c4d338115c87c7ab747271
Kenny Yokoyama <kyokoyam@ucsd.edu>
2014-03-26 14:58:01 -0700

    updated Graph.cpp and Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -13,49 +13,74 @@
 using namespace std;
 using namespace BotConsts;
 
-class Node{
+class Edge{
   private:
-    int index;
-    vector<Node*> nodes;
+    int from;
+    int to;
     int cost;
     bool checked;
 
   public:
-    Node(int cor, int cost):index(cor), cost(cost){}
-    void setIndex(int idx){ index = idx; }
-    int getIndex(int idx){ return index; }
+    Edge():from(-1),to(-1),cost(256),checked(false){}
+    Edge(int f, int t):from(f),to(t),cost(256),checked(false){}
+    Edge(int f, int t, int c):from(f),to(t),cost(c),checked(false){}
 
+    int getTo(int f) const { return to; }
+    int getCost() const { return cost; }
+    void setCost(int c) { cost = c; }
+    bool isChecked() const { return checked; }
+    void setChecked(bool c){ checked = c; }
+}
+
+class Node{
+  private:
+    int index;
+    vector<Edge*> edges;
+    int cost;
+    bool checked;
+    int from;
+
+  public:
+    Node():index(-1),cost(256),checked(false),from(-1){}
+    Node(int i, int c):index(i), cost(c){}
+    ~Node();
+
+    int getIndex() const { return index; }
+    void addEdge(Edge* e);
+    const vector<Edge*> &getEdges() const { return edges; }
+    int getCost() const { return cost; }
+    void setCost(int c) { cost = c; }
+    bool getChecked() const { return checked; }
+    void setChecked(bool c) { checked = c; }
+    int getFrom() const { return from; }
+    void setFrom(int f) { from = f; }
 };
 
 
 class Graph{
-    private: 
-        Node* start;
-        vector<Node*> nodes;
-        // vector<Edge*> edges;
+  private: 
+    Node* start;
+    vector<Edge*> edges;
+    vector<Node*> nodes;
 
-    public:
-        /*
-         * Default Constructor
-         */
-        Graph()
-        {
-          for(int i=0; i<BOARD_MAX*BOARD_MAX; i++)
-          {
-            nodes[i]->setIndex(BOARD_MAX*BOARD_MAX);
-          }
-          start = nodes[(BOARD_MAX-1)*(BOARD_MAX-1) + 0];
-        }
+  public:
+    /*
+     * Default Constructor
+     */
+    Graph()
+    {
+      for(int i=0; i<BOARD_MAX*BOARD_MAX; i++)
+      {
+        nodes[i]->setIndex(BOARD_MAX*BOARD_MAX);
+      }
+      start = nodes[(BOARD_MAX-1)*(BOARD_MAX-1) + 0];
+    }
 
-
-        /*
-         * From the traversal matrix, create a graph
-         */
-        void mtxToGraph();
-
-
-        void printGraph();
-
+    void loadMaze(const int[][BOARD_MAX] maze);
+    void dijkstra(int start, int end, bool isSearching);
+    void astar(int start, int end, bool isSearching); 
+    const Route getRoute(int start, int end) const;
+    void printGraph();
 };
 
 
--- dijkstra.cpp
+++ dijkstra.cpp
@@ -43,6 +43,38 @@ void dijkstraMatrix(Traversal trav, Navigation nav){
 }
 
 /*
+ * Check how many cells the bot can move till it reaches a cell where it can 
+ * change direction
+ * Input: 
+ * Output: 
+ */
+int checkNextTurn(int cur_x, int cur_y, int dir, Traversal trav){
+  int count = 0;
+  int x_wk = cur_x;
+  int y_wk = cur_y;
+  int mask;
+  int inc_x;
+  int inc_y;
+
+  // Determine mask to use based on the current direction
+  switch(dir){
+    case 0: mask = 0xFB; inc_x=0; inc_y=-1; break; //1011
+    case 1: mask = 0xF7; inc_x=1; inc_y=0; break; //0111
+    case 2: mask = 0xFE; inc_x=0; inc_y=1; break; //1110
+    case 3: mask = 0xFD; inc_x=-1; inc_y=0; break; //1101
+  }
+
+  // Calculate number of cells
+  while((trav.getTraversalVal(x_wk, y_wk)&0x0F) == mask){
+    x_wk += inc_x;
+    y_wk += inc_y;
+    count++;
+  }
+
+  return count;
+}
+
+/*
  * Run Dijkstra's Algorithm on a board represented as a graph 
  * Input:  
  * Output: 
