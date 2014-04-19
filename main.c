0554d898c60acc3c814778a4b6726fd1eb471955
Apo11oH <kyokoyam@ucsd.edu>
2014-04-19 12:07:04 -0700

    project structure update part 2. pwm working & configuring

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stm32f4xx.h>
 #include <stdio.h>
 
-#define TIM2_CLKFREQ 1000UL
+#define TIM2_CLKFREQ 10000UL
 
 GPIO_InitTypeDef  GPIO_InitStruct;
 TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
@@ -43,12 +43,13 @@ void initMotors()
 	// Enable peripheral clock
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 	// Enable GPIO pins for High
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_12 | GPIO_Pin_15;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_Init(GPIOB, &GPIO_InitStruct);
+	/*
 	// Enable GPIO pins for Low
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_15;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
@@ -56,6 +57,7 @@ void initMotors()
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
 	GPIO_Init(GPIOB, &GPIO_InitStruct);
+	*/
 }
 
 void initPWM()
@@ -72,7 +74,7 @@ void initPWM()
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOC, &GPIO_InitStruct);
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 	// Connect TIM3 pins to AF2
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_TIM3);
@@ -111,9 +113,9 @@ void pwm_config(int period){
 
 void pwm_set_power(uint16_t ch, uint16_t dutycycle){
 	switch(ch){
-	case 1: TIM3->CCR1 = dutycycle; break;
-	case 2: TIM3->CCR2 = dutycycle; break;
-	default: break;
+		case 1: TIM3->CCR1 = dutycycle; break;
+		case 2: TIM3->CCR2 = dutycycle; break;
+		default: break;
 	}
 }
 
@@ -122,13 +124,17 @@ int main(void)
 	uint16_t pulse_width = 0;
 
 	initPins();
-	pwm_config(800);
 
-	pwm_set_power(1,100);
-	pwm_set_power(2,100);
+	GPIOB->BSRRL = 0x6000; //0110 0000 0000 0000
+	GPIOB->BSRRH = 0x9000; //1001 0000 0000 0000
+	pwm_config(100);
+
+	pwm_set_power(1,50);
+	pwm_set_power(2,50);
 
     while(1)
     {
+    	/*
     	pwm_set_power(1,pulse_width++);
     	pwm_set_power(2,pulse_width++);
         if (pulse_width > 800)
@@ -139,5 +145,6 @@ int main(void)
         	GPIO_ToggleBits(GPIOB, GPIO_Pin_11);
         	pulse_width = 0;
         }
+        */
     }
 }
